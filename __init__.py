cd9410503066833055815307fca740ee1d16f116
David X <66816038+UnitedRecord@users.noreply.github.com>
2020-06-14 09:30:25 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,12 @@
-##version = '20200612'
+##version = v0.1
+##what is 
+##create value from here
+##we bielieve the thing that we need to judge that what is value
 
+def __init__(self, k):
+  record = []
+  UnitedRecord = []
+  Record = []
+
+def main():
+  
